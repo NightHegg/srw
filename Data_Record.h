f76952cae0680756e8dbb4fe659629d3f6166959
NightHegg <61695767+NightHegg@users.noreply.github.com>
2020-03-24 12:51:00 +0300

    Record changes/Change workplace

--- Data_Record.h
+++ Data_Record.h
@@ -2,20 +2,22 @@
 
 #include <fstream>
 #include "Classes.h"
-
+/**
+ * TODO: Recreate this function for every class
+ */
 void Record_Results(Vector& y, Matrix& Sigma, double uk, double rk)
 {
     ofstream outfile("results/y.dat");
     for (int i=0;i<y.iV;i++)
     {
-        outfile <<y.V[i] *uk;
+        outfile <<y.V[i] * uk;
         outfile <<endl;
     }
     outfile.close();
     ofstream outfile_second("results/Sigma.dat");
     for (int j=0;j<Sigma.jM;j++)
     {
-        outfile_second<<Sigma.M[0][j]*rk<<" "<<Sigma.M[1][j]*rk;
+        outfile_second<<Sigma.M[0][j] * rk<<" "<<Sigma.M[1][j] * rk;
         outfile_second<<endl;
     }
     outfile_second.close();
