3e190477c6034b65af086cad44bad3bc3a46d936
NightHegg <61695767+NightHegg@users.noreply.github.com>
2020-04-03 15:39:16 +0300

    Changed CoefOverflow system, updated desicions

--- Data_Record.h
+++ Data_Record.h
@@ -12,15 +12,23 @@ void Record_Results(Vector& y, Matrix& Sigma, double uk, double rk, int Amount_S
 {
     std::string sep="_";
     std::string size=std::to_string(Sigma.GetSize_j());
+    if (Sigma.GetSize_j()<10)
+    {
+        size="00"+size;
+    }
+    else if (Sigma.GetSize_j()>=10 && Sigma.GetSize_j()<100)
+    {
+        size="0"+size;
+    }
     std::string AS=std::to_string(Amount_Subdomains);
-    ofstream outfile(Route+size+sep+AS+sep+"y"+".dat");
+    ofstream outfile(Route+"y"+sep+size+sep+AS+".dat");
     for (int i=0;i<y.GetSize();i++)
     {
         outfile <<y.GetElement(i) * uk;
         outfile <<endl;
     }
     outfile.close();
-    ofstream outfile_second(Route+size+sep+AS+sep+"Sigma"+".dat");
+    ofstream outfile_second(Route+"Sigma"+sep+size+sep+AS+".dat");
     for (int j=0;j<Sigma.GetSize_j();j++)
     {
         outfile_second<<Sigma.GetElement(0,j) * rk<<" "<<Sigma.GetElement(1,j) * rk;
@@ -31,9 +39,18 @@ void Record_Results(Vector& y, Matrix& Sigma, double uk, double rk, int Amount_S
 
 void Record_AddData(int N, int Amount_Subdomains, int Counter, double stopCriteria, double Coef_Overflow, string Route)
 {
+    std::string sep="_";
     std::string size=std::to_string(N);
+    if (N<10)
+    {
+        size="00"+size;
+    }
+    else if (N>=10 && N<100)
+    {
+        size="0"+size;
+    }
     std::string AS=std::to_string(Amount_Subdomains);
-    ofstream ofile(Route+size+"_"+AS+"_addData.dat");
+    ofstream ofile(Route+"AddData"+sep+size+sep+AS+".dat");
     ofile<<Amount_Subdomains;
     ofile<<endl;
     ofile<<Counter;
